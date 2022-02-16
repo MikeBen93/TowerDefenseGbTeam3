2f6df668fc2a3a59c4dbdeb712d7a677a51a5b51
MikeBen93 <m.benkov93@gmail.com>
2022-02-16 20:39:28 +0300

    added particle system to red hearts, added sprites to hexagones and changed enemies movement@

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,18 +7,29 @@ public class EnemyMovement : MonoBehaviour
     private Transform _target;
     private int _waypointIndex = 0;
 
+    private Vector3 _dir;
+    private Quaternion _lookRotation;
+    private Vector3 _rotation;
+
     private Enemy _enemy;
 
     private void Start()
     {
         _target = Waypoints.waypoints[_waypointIndex];
+
+        _dir = _target.position - transform.position;
+        _lookRotation = Quaternion.LookRotation(_dir);
+
         _enemy = GetComponent<Enemy>();
     }
 
     private void Update()
     {
         Vector3 direction = _target.position - transform.position;
+        _rotation = Quaternion.Lerp(transform.rotation, _lookRotation, Time.deltaTime * 10).eulerAngles;
+
         transform.Translate(direction.normalized * _enemy.speed * Time.deltaTime, Space.World);
+        transform.rotation = Quaternion.Euler(0f, _rotation.y, 0f);
 
         if (ReachedWaypoint()) GetNextWaypoint();
 
@@ -47,6 +58,8 @@ public class EnemyMovement : MonoBehaviour
 
         _waypointIndex++;
         _target = Waypoints.waypoints[_waypointIndex];
+        _dir = _target.position - transform.position;
+        _lookRotation = Quaternion.LookRotation(_dir);
     }
 
     private void EndPath()
