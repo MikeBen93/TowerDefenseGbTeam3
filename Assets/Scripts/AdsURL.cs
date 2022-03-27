5d505e59151d9d9581e8135e3d1ae51b8c52c65b
Ve9tamr <smilekamch@mail.ru>
2022-03-28 02:29:19 +1200

    AddedAds

---
+++ Assets/Scripts/AdsURL.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdsURL : MonoBehaviour
+{
+    public void OpenDiscord()
+    {
+        Application.OpenURL("https://discord.gg/kYWEY2jaXN");
+    }
+}
